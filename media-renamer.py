f9befa01c5cdc929c33ef1425b54cb73196b24cf
Matt Parlette <matthew.parlette@gmail.com>
2013-01-28 20:23:33 -0500

    Added some helper log functions.
    Added a log_debug and log_error to standardize the output to the screen for these message types.

--- media-renamer.py
+++ media-renamer.py
@@ -2,6 +2,20 @@
 
 from argparse import ArgumentParser
 
+def log_debug(message,identifier = ""):
+  if args.debug:
+    if identifier:
+      print "Debug:\t%s:\t%s" % (identifier,message)
+    else:
+      print "Debug:\t\t%s" % (message)
+
+def log_error(message,identifier = ""):
+  if args.debug:
+    if identifier:
+      print "Error:\t%s:\t%s" % (identifier,message)
+    else:
+      print "Error:\t\t%s" % (message)
+
 argparser = ArgumentParser("Rename media files")
 argparser.add_argument('-d','--directory',help="Directory to process",nargs=1)
 argparser.add_argument('-t','--type',help="Specify the type of media being processed",nargs=1)
@@ -9,3 +23,4 @@ argparser.add_argument('-i','--id',help="ID for the show or movie on thetvdb or 
 argparser.add_argument('--debug',help="Display debug messages",action='store_true')
 
 args = argparser.parse_args()
+
