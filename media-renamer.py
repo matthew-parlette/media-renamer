e0587571b458c9dca36a00529cb9d258a58f38d7
Matt Parlette <matthew.parlette@gmail.com>
2013-01-29 13:05:14 -0500

    Determine working directory and media type.
    If the working directory is not provided from the command line, then it is defaulted to the current working directory.
    
    If the media type is not provided, then the user is prompted for it.
    
    This commit also adds a simple menu system for getting a user selection from a list.

--- media-renamer.py
+++ media-renamer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
 from argparse import ArgumentParser
+from os.path import abspath
+from os import getcwd
+
+modes = ['tv','movie']
 
 def log_debug(message,identifier = ""):
   if args.debug:
@@ -16,6 +20,56 @@ def log_error(message,identifier = ""):
     else:
       print "Error:\t\t%s" % (message)
 
+## {{{ http://code.activestate.com/recipes/134892/ (r2)
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+## end of http://code.activestate.com/recipes/134892/ }}}
+
+def menu(title,options):
+  while True:
+    print title
+    print '=' * len(title)
+    for i,option in enumerate(options):
+      print "%s. %s" % (str(i),option)
+    selection = int(getch())
+    if selection < len(options):
+      return options[selection]
+    else:
+      print "Invalid selection"
+
 argparser = ArgumentParser("Rename media files")
 argparser.add_argument('-d','--directory',help="Directory to process",nargs=1)
 argparser.add_argument('-t','--type',help="Specify the type of media being processed",nargs=1)
@@ -23,4 +77,22 @@ argparser.add_argument('-i','--id',help="ID for the show or movie on thetvdb or 
 argparser.add_argument('--debug',help="Display debug messages",action='store_true')
 
 args = argparser.parse_args()
+
+getch = _Getch()
+
+#Get working directory
+if args.directory:
+  wd = abspath(args.directory[0])
+  log_debug("Working directory read from command line as %s" % wd)
+else:
+  wd = abspath(getcwd())
+  log_debug("Working directory defaulted to current directory of %s" % wd)
+
+#Determine mode
+if args.type and args.type in modes:
+  mode = args.type[0]
+  log_debug("Media type set from command line as %s" % mode)
+else:
+  mode = menu("Media Type",options=modes)
+  log_debug("Media type set from user selection as %s" % mode)
 
